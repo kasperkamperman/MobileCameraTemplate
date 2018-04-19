0db2f16884ecd1bc6ca24c095cd28659fb544f66
Kasper Kamperman <kasperkamperman@gmail.com>
2018-04-19 14:02:08 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# MobileFirstCameraTemplate
+# Mobile (and Desktop) Camera Template (HTML, CSS, JS)
+
+I like to experiment with Computer Vision and AI API's (like Azure Cognetive Services, Google Cloud Vision, IBM Watson) to see if
+I can utilize them for certain solutions. 
+
+The most easy way to test them is directly making a photo and sending image data to API script, instead of uploading files. 
+
+I didn't find a fast mobile first camera template for HTML5 as a starting point for my prototypes, so I developed one myself. 
+The interface setup is mainly inspired by the standard Android and iOS Cameras. 
+
+The template doesn't do anything with the image(canvas) data yet, I'll leave that up to you. 
+
+Feel free to use it in your next Computer Vision or AI project. 
+
+### Requirements
+
+- WebRTC is only supported on secure connections. So you need to serve it from https. 
+*You can test and debug in Chrome from localhost though (this doesn't work in Safari).* 
+
+- A recent OS and browser. It should work on recent Phones and OS-es. If it isn't, please 
+  let me know (issue) (including a suggestion to fix it). Also add the debugging info in the console.
+
+### Functionalities
+
+- Fullscreen mode
+- Take Photo
+- Flip Camera (environment / user)
+
+**Check the [demo](https://demo.kasperkamperman.com/mobilecamtemplate/)**
+
+## Used Libraries:
+
+- Fullscreen functionality: [Screenfull.js](https://github.com/sindresorhus/screenfull.js/)
+- Detect WebRTC support: [DetectRTC.js](https://www.webrtc-experiment.com/DetectRTC/)
+- WebRTC cross-browser: [Adapter.js](https://github.com/webrtc/adapter)
+- UI click sound: [Howler.js](https://howlerjs.com)
+
+## Used Assets:
+
+- ("Basic Click Wooden")[https://freesound.org/people/GameAudio/sounds/220200/] - by (GameAudio)[https://www.gameaudio101.com]
+- [Material Design Icons] (https://material.io/icons/) (camera front, camera rear, photo camera, fullscreen, fullscreen exit)
+
+## Tested with:
+
+- Chrome Android 8.0 (Nokia 5)
+- Chrome 65 - MacOs 10.12.6
+- Safari 11.0.3 - MacOs 10.12.6
+
+## Created by 
+
+[Kasper Kamperman](https://www.kasperkamperman.com/)
+
+Credits and a link to my website are always appreciated. 
+I'm always curious how people end up using my stuff, so
+feel free to [mail](https://www.kasperkamperman.com/) or [send a tweet to me](https://twitter.com/kasperkamperman). (@kasperkamperman). 
+
+## Good WebRTC resources
+
+- https://webrtc.github.io/samples/
+- https://www.webrtc-experiment.com/
+- https://www.html5rocks.com/en/tutorials/getusermedia/intro/
+- https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
---
+++ js/main.js
@@ -0,0 +1,296 @@
+/*
+
+A mobile first camera template. 
+
+I like to experiment with Computer Vision and AI API's (like Azure Cognetive Services, Google Cloud Vision, IBM Watson) to see if
+I can utalize them for certain solutions. 
+
+The most easy way to test them is directly making a photo and sending image data. I didn't find a fast
+mobile first camera template for HTML5 as a starting point for my prototypes, so I developed one myself. 
+
+The template doesn't do anything with the image data yet, I'll leave that up to you. 
+
+Functionalities
+- Fullscreen mode
+- Take Photo
+- Flip Camera (environment / user)
+
+
+
+
+A camera template to interface with a camera of a mobile phone (although desktop webcam works as well) by using WebRTC.
+You could use it as a starting point for computer vision scripts and API's. 
+Interface is mainly inspired by the Android 8 Camera. 
+
+>> explanations and WebRTC examples
+
+    https://www.html5rocks.com/en/tutorials/getusermedia/intro/
+    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    https://webrtc.github.io/samples/ (mooie voorbeelden)
+
+>> scripts used: 
+
+    https://github.com/sindresorhus/screenfull.js/
+    https://www.webrtc-experiment.com/DetectRTC/
+    https://github.com/webrtc/adapter
+    https://howlerjs.com
+  
+>> kasperkamperman.com - 2018-04-18
+
+*/
+
+var takeSnapshotUI = createClickFeedbackUI();
+
+var video;
+var takePhotoButton;
+var toggleFullScreenButton;
+var switchCameraButton;
+var amountOfCameras = 0;
+var currentFacingMode = 'environment';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // do some WebRTC checks before creating the interface
+    DetectRTC.load(function() {
+
+        // do some checks
+        if (DetectRTC.isWebRTCSupported == false) {
+            alert('Please use Chrome, Firefox or iOS 11 and macOS 10.13.');
+        }
+        else {
+            if (DetectRTC.hasWebcam == false) {
+                alert('Please install an external webcam device.');
+            }
+            else {
+
+                amountOfCameras = DetectRTC.videoInputDevices.length;
+                       
+                initCameraUI();
+                initCameraStream();
+            } 
+        }
+        
+        console.log("RTC Debug info: " + 
+            "\n OS:                   " + DetectRTC.osName + " " + DetectRTC.osVersion + 
+            "\n browser:              " + DetectRTC.browser.fullVersion + " " + DetectRTC.browser.name +
+            "\n is Mobile Device:     " + DetectRTC.isMobileDevice +
+            "\n has webcam:           " + DetectRTC.hasWebcam + 
+            "\n has permission:       " + DetectRTC.isWebsiteHasWebcamPermission +       
+            "\n getUserMedia Support: " + DetectRTC.isGetUserMediaSupported + 
+            "\n isWebRTC Supported:   " + DetectRTC.isWebRTCSupported + 
+            "\n WebAudio Supported:   " + DetectRTC.isAudioContextSupported +
+            "\n is Mobile Device:     " + DetectRTC.isMobileDevice
+        );
+
+    });
+
+});
+
+function initCameraUI() {
+    
+    video = document.getElementById('video');
+
+    takePhotoButton = document.getElementById('takePhotoButton');
+    toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
+    switchCameraButton = document.getElementById('switchCameraButton');
+    
+    // https://developer.mozilla.org/nl/docs/Web/HTML/Element/button
+    // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role
+
+    takePhotoButton.addEventListener("click", function() {
+        takeSnapshotUI();
+        takeSnapshot();        
+    });
+
+    // -- fullscreen part
+
+    function fullScreenChange() {
+        if(screenfull.isFullscreen) {
+            toggleFullScreenButton.setAttribute("aria-pressed", true);
+        }
+        else {
+            toggleFullScreenButton.setAttribute("aria-pressed", false);
+        }
+    }
+
+    if (screenfull.enabled) {
+        screenfull.on('change', fullScreenChange);
+
+        toggleFullScreenButton.style.display = 'block';  
+
+        // set init values
+        fullScreenChange();
+
+        toggleFullScreenButton.addEventListener("click", function() {
+            screenfull.toggle(document.getElementById('container'));
+        });
+    }
+        
+    // -- switch camera part
+    if(amountOfCameras > 1) {
+        
+        switchCameraButton.style.display = 'block';
+        
+        switchCameraButton.addEventListener("click", function() {
+
+            if(currentFacingMode === 'environment') currentFacingMode = 'user';
+            else                                    currentFacingMode = 'environment';
+
+            initCameraStream();
+
+        });  
+    }
+
+    // Listen for orientation changes to make sure buttons stay at the side of the 
+    // physical (and virtual) buttons (opposite of camera)
+    // most is done by CSS media queries
+    // http://www.williammalone.com/articles/html5-javascript-ios-orientation/
+    // https://www.sitepoint.com/introducing-screen-orientation-api/
+
+    window.addEventListener("orientationchange", function() {
+        // console.log(screen.orientation.angle);
+        // window.orientation.angle;
+        var angle = screen.orientation.angle; 
+
+        var guiControls = document.getElementById("gui_controls").classList;
+        var vidContainer = document.getElementById("vid_container").classList;
+
+        if(angle == 270 || angle == -90) {
+            guiControls.add('left');
+            vidContainer.add('left');
+
+        }
+        else {
+            if ( guiControls.contains('left') ) guiControls.remove('left');
+            if ( vidContainer.contains('left') ) vidContainer.remove('left');
+        }
+
+        //0   portrait-primary   
+        //180 portrait-secondary device is down under
+        //90  landscape-primary  buttons at the right
+        //270 landscape-secondary buttons at the left
+    }, false);
+    
+}
+
+
+
+// https://github.com/webrtc/samples/blob/gh-pages/src/content/devices/input-output/js/main.js
+function initCameraStream() {
+
+    // stop any active streams in the window
+    if (window.stream) {
+        window.stream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+
+    var constraints = { 
+        audio: false, 
+        video: {
+            //width: { min: 1024, ideal: window.innerWidth, max: 1920 },
+            //height: { min: 776, ideal: window.innerHeight, max: 1080 },
+            facingMode: currentFacingMode
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);   
+
+    function handleSuccess(stream) {
+
+        window.stream = stream; // make stream available to browser console
+        video.srcObject = stream;
+
+        if(constraints.video.facingMode) {
+
+            if(constraints.video.facingMode === 'environment') {
+                switchCameraButton.setAttribute("aria-pressed", true);
+            }
+            else {
+                switchCameraButton.setAttribute("aria-pressed", false);
+            }
+        }
+
+        return navigator.mediaDevices.enumerateDevices();
+    }
+
+    function handleError(error) {
+
+        console.log(error);
+
+        //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        if(error === 'PermissionDeniedError') {
+            alert("Permission denied. Please refresh and give permission.");
+        }
+        
+    }
+
+}
+
+function takeSnapshot() {
+    
+    // if you'd like to show the canvas add it to the DOM
+    var canvas = document.createElement('canvas');
+
+    var width = video.videoWidth;
+    var height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width, height);
+
+    // polyfil if needed https://github.com/blueimp/JavaScript-Canvas-to-Blob
+    
+    // https://developers.google.com/web/fundamentals/primers/promises
+    // https://stackoverflow.com/questions/42458849/access-blob-value-outside-of-canvas-toblob-async-function
+    function getCanvasBlob(canvas) {
+        return new Promise(function(resolve, reject) {
+            canvas.toBlob(function(blob) { resolve(blob) }, 'image/jpeg');
+        })
+    }
+
+    // some API's (like Azure Custom Vision) need a blob with image data
+    getCanvasBlob(canvas).then(function(blob) {
+
+        // do something with the image blob
+
+    });
+
+}
+
+// https://hackernoon.com/how-to-use-javascript-closures-with-confidence-85cd1f841a6b
+// closure; store this in a variable and call the variable as function
+// eg. var takeSnapshotUI = createClickFeedbackUI();
+// takeSnapshotUI();
+
+function createClickFeedbackUI() {
+
+    // in order to give feedback that we actually pressed a button. 
+    // we trigger a almost black overlay
+    var overlay = document.getElementById("video_overlay");//.style.display;
+
+    // sound feedback
+    var sndClick = new Howl({ src: ['snd/click.mp3'] });
+
+    var overlayVisibility = false;
+    var timeOut = 80;
+
+    function setFalseAgain() {
+        overlayVisibility = false;	
+        overlay.style.display = 'none';
+    }
+
+    return function() {
+
+        if(overlayVisibility == false) {
+            sndClick.play();
+            overlayVisibility = true;
+            overlay.style.display = 'block';
+            setTimeout(setFalseAgain, timeOut);
+        }   
+
+    }
+}
