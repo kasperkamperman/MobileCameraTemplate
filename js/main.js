d71104bc0ac387f62fcb082e112ebed78956bab4
Or Argaman <orargaman@gmail.com>
2019-05-26 12:09:18 +0300

    Change to 0.8

--- js/main.js
+++ js/main.js
@@ -232,7 +232,7 @@ function takeSnapshot() {
             "Content-Type": "text/plain",
             "cache-control": "no-cache"
         },
-        "data": canvas.toDataURL("image/jpeg", 1.0).match(imgDataUrlregex)[2]
+        "data": canvas.toDataURL("image/jpeg", 0.8).match(imgDataUrlregex)[2]
     }
 
     $.ajax(settings).done(function (response) {
