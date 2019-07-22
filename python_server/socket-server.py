30420d6afc495f5d8a94d59d28a9c9abbb40aede
Mordechai Alter <moralt@microsoft.com>
2019-07-22 23:34:25 +0300

    delete comments

--- python_server/socket-server.py
+++ python_server/socket-server.py
@@ -28,39 +28,3 @@ def test_disconnect():
 
 if __name__ == '__main__':
     socketio.run(app, port=5000)
-
-
-# #!/usr/bin/python
-# import websocket
-# import thread
-# import time
-
-# def on_message(ws, message):
-#     print ("on_message: " + message)
-
-# def on_error(ws, error):
-#     print ("on_error: " + str(error))
-
-# def on_close(ws):
-#     print ("### closed ###")
-
-# def on_open(ws):
-#     def run(*args):
-#         for i in range(30000):
-#             time.sleep(1)
-#             #ws.send("Hello %d" % i)
-#         time.sleep(1)
-#         ws.close()
-#         print ("thread terminating...")
-#     thread.start_new_thread(run, ())
-
-
-# if __name__ == "__main__":
-#     websocket.enableTrace(True)
-#     ws = websocket.WebSocketApp("ws://localhost:5000/",
-#                                 on_message = on_message,
-#                                 on_error = on_error,
-#                                 on_close = on_close)
-#     ws.on_open = on_open
-
-#     ws.run_forever()
